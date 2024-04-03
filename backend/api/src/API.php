17fbac8b4f7262ffbfef9c57a99725161a215b23
svanve <silvanvanveen@gmail.com>
2024-04-03 11:25:01 +0200

    fix issue in browser regarding access-control-allow-headers: *

--- backend/api/src/API.php
+++ backend/api/src/API.php
@@ -20,7 +20,7 @@ final class API {
     public function run() : void {
         
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Headers: *');
+        header('Access-Control-Allow-Headers: Authorization');
         header('Access-Control-Allow-Methods: *');
         header('Access-Control-Allow-Credentials: true');    
 
